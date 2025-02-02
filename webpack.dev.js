a51f46a5d4ef0d842397b11c6c33197178fec5c5
Isabella <isabellafj97@gmail.com>
2025-02-02 13:28:25 -0600

    use correct webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
+const common = require("./webpack.config.js");
 const Dotenv = require("dotenv-webpack");
 
 module.exports = async (env, argv) => {
