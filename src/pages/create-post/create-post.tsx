1f907ded5ffaf08892802441122fb9987e971683
Isabella <isabellafj97@gmail.com>
2025-02-09 20:01:56 -0600

    fix merge

--- src/pages/create-post/create-post.tsx
+++ src/pages/create-post/create-post.tsx
@@ -11,6 +11,7 @@ import globalStyles from "shared/styles";
 import { useAppSelector } from "redux_store/hooks";
 import { useState } from "react";
 import GiftType from "./gift-type";
+import { useNavigation } from "@react-navigation/native";
 
 type FormValues = {
   title: string;
@@ -21,12 +22,15 @@ type FormValues = {
 };
 
 export default function CreatePost() {
+  const navigation = useNavigation();
   const form: UseFormReturn<FormValues> = useForm<FormValues>();
-  const token = useAppSelector((state) => state.auth.token);
-  const [tags, setTags] = useState('');
+  const [showLoginForm, setShowLoginForm] = useState(false);
+  const { token, isLoggedIn } = useAuth();
+  const [tags, setTags] = useState("");
   const [files, setFiles] = useState([]);
-  const [postType, setPostType] = useState('gift');
-  const [giftType, setGiftType] = useState('Gift');
+  const [postType, setPostType] = useState("gift");
+  const [giftType, setGiftType] = useState("Gift");
+  const { addPost } = usePosts();
 
   const onInvalid = (e) => {
     console.error(e);
@@ -58,113 +62,154 @@ export default function CreatePost() {
     form.reset();
   };
 
+  if (showLoginForm) {
+    <View style={styles.loginWrapper}>
+      <Text style={styles.loginPrompt}>Please log in to create a post.</Text>
+      <Login onSuccess={handleLoginSuccess} />
+    </View>;
+  }
+
   return (
     <>
-    <View>
-      <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', rowGap: 10, marginHorizontal: 16, gap: 3, marginTop: 10 }}>
-      <Button 
-      style={{
-        ...globalStyles.button, 
-        backgroundColor: postType === 'gift' ? 'black' : '#c5c5c5',  
-        borderColor: '#444', 
-        borderWidth: 1, 
-        paddingHorizontal: 30
-      }}
-      labelStyle={{ color: postType === 'gift' ? 'white' : 'black' }}  
-      onPress={() => setPostType('gift')}
-      >
-        Give
-      </Button>
-
-      <Button 
-        style={{
-          ...globalStyles.button, 
-          backgroundColor: postType === 'request' ? 'black' : '#c5c5c5',  
-          paddingHorizontal: 30,
-          borderColor: '#444',
-          borderWidth: 1
-        }}
-        labelStyle={{ color: postType === 'request' ? 'white' : 'black' }}  
-        onPress={() => setPostType('request')}
-      >
-        Request
-      </Button>
-
-      <Image
-        source={{ uri: 'https://cdn-icons-png.flaticon.com/512/25/25231.png' }}
-        alt="Add Image"
-        style={{ width: 32, height: 32 }}
-      />
-      </View>
-      <View style={globalStyles.contentContainerWithMargin}>
-        <View style={{width: '90%', marginVertical: 32}}>
-          {postType === 'gift' && (
-            <GiftType selected={giftType} onSelect={setGiftType}/>
-          )}
-          <Text style={{...globalStyles.inputTitle, marginTop: 10}}>Title</Text>
-          <TextInput 
-            value={form.watch("title")} 
-            onChangeText={text => form.setValue("title", text)} 
-            style={globalStyles.inputForm} 
-            onInvalid={onInvalid} 
-            placeholder="Enter title"
-          />
-          
-          <Text style={globalStyles.inputTitle}>Info</Text>
-          <TextInput 
-            value={form.watch("body")} 
-            onChangeText={text => form.setValue("body", text)} 
-            style={globalStyles.inputForm} 
-            multiline 
-            numberOfLines={4} 
-            placeholder="Enter post info"
-          />
-          
-          <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-            <Text style={globalStyles.inputTitle}>Add Images</Text>
-            <TouchableOpacity>
-              <Image
-                source={{ uri: 'https://cdn-icons-png.flaticon.com/512/25/25231.png' }}
-                alt="Add Image"
-                style={{ width: 50, height: 50, marginLeft: 10 }} 
-              />
-            </TouchableOpacity>
-          </View>
-          
-          <Text style={globalStyles.inputTitle}>Tags</Text>
-          <TextInput 
-            value={form.watch("tags")} 
-            onChangeText={text => form.setValue("tags", text)} 
-            style={globalStyles.inputForm} 
-            onInvalid={onInvalid} 
-            placeholder="Enter tags" 
-          />
-          
-          <Text style={globalStyles.inputTitle}>Location</Text>
-          <View>
-            <Image 
-              source={{ uri: 'https://cdn-icons-png.flaticon.com/512/25/25231.png' }} 
-              alt="Location" 
-              style={{ width: 50, height: 50 }} 
-            />
-          </View>
-          
-          <Button 
-            mode="contained" 
-            style={globalStyles.button} 
-            onPress={form.handleSubmit(onSubmit)}
+      <View>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+            alignItems: "center",
+            rowGap: 10,
+            marginHorizontal: 16,
+            gap: 3,
+            marginTop: 10,
+          }}
+        >
+          <Button
+            style={{
+              ...globalStyles.button,
+              backgroundColor: postType === "gift" ? "black" : "#c5c5c5",
+              borderColor: "#444",
+              borderWidth: 1,
+              paddingHorizontal: 30,
+            }}
+            labelStyle={{ color: postType === "gift" ? "white" : "black" }}
+            onPress={() => setPostType("gift")}
           >
-            Create
+            Give
           </Button>
+
+          <Button
+            style={{
+              ...globalStyles.button,
+              backgroundColor: postType === "request" ? "black" : "#c5c5c5",
+              paddingHorizontal: 30,
+              borderColor: "#444",
+              borderWidth: 1,
+            }}
+            labelStyle={{ color: postType === "request" ? "white" : "black" }}
+            onPress={() => setPostType("request")}
+          >
+            Request
+          </Button>
+
+          <Image
+            source={{
+              uri: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+            }}
+            alt="Add Image"
+            style={{ width: 32, height: 32 }}
+          />
+        </View>
+        <View style={globalStyles.contentContainerWithMargin}>
+          <View style={{ width: "90%", marginVertical: 32 }}>
+            {postType === "gift" && (
+              <GiftType selected={giftType} onSelect={setGiftType} />
+            )}
+            <Text style={{ ...globalStyles.inputTitle, marginTop: 10 }}>
+              Title
+            </Text>
+            <TextInput
+              value={form.watch("title")}
+              onChangeText={(text) => form.setValue("title", text)}
+              style={globalStyles.inputForm}
+              //onInvalid={onInvalid}
+              placeholder="Enter title"
+            />
+
+            <Text style={globalStyles.inputTitle}>Info</Text>
+            <TextInput
+              value={form.watch("body")}
+              onChangeText={(text) => form.setValue("body", text)}
+              style={globalStyles.inputForm}
+              multiline
+              numberOfLines={4}
+              placeholder="Enter post info"
+            />
+
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <Text style={globalStyles.inputTitle}>Add Images</Text>
+              <TouchableOpacity>
+                <Image
+                  source={{
+                    uri: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+                  }}
+                  alt="Add Image"
+                  style={{ width: 50, height: 50, marginLeft: 10 }}
+                />
+              </TouchableOpacity>
+
+              <View style={globalStyles.formRow}>
+                <Input
+                  name="files"
+                  label="Attach Files"
+                  type="file"
+                  form={form}
+                  registerOptions={{ required: false }}
+                />
+              </View>
+            </View>
+
+            <Text style={globalStyles.inputTitle}>Tags</Text>
+            <TextInput
+              // value={form.watch("tags")}
+              // onChangeText={(text) => form.setValue("tags", text)}
+              style={globalStyles.inputForm}
+              // onInvalid={onInvalid}
+              placeholder="Enter tags"
+            />
+
+            <Text style={globalStyles.inputTitle}>Location</Text>
+            <View>
+              <Image
+                source={{
+                  uri: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+                }}
+                alt="Location"
+                style={{ width: 50, height: 50 }}
+              />
+            </View>
+
+            <Button
+              mode="contained"
+              style={globalStyles.button}
+              onPress={form.handleSubmit(onSubmit)}
+            >
+              Create
+            </Button>
+          </View>
         </View>
       </View>
-    </View>
-  </>
-  
+    </>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = {
   loginWrapper: {
     padding: 20,
   },
@@ -172,4 +217,4 @@ const styles = StyleSheet.create({
     marginBottom: 15,
     fontSize: 16,
   },
-});
+};
