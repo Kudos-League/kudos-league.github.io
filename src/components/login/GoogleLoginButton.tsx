dc9f6fcce70a461aef934643b6f3876965557b18
Isabella <isabellafj97@gmail.com>
2025-05-31 16:55:22 -0500

    google login button

--- src/components/login/GoogleLoginButton.tsx
+++ src/components/login/GoogleLoginButton.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { getEndpointUrl } from '@/shared/api/config';
 
 export default function DiscordLoginButton() {
-    const handleDiscordLogin = () => {
-        const discordLoginUrl = `${getEndpointUrl()}/users/discord`;
-        window.location.href = discordLoginUrl;
+    const handleLogin = () => {
+        const loginURL = `${getEndpointUrl()}/users/google`;
+        window.location.href = loginURL;
     };
 
     return (
         <button
-            onClick={handleDiscordLogin}
+            onClick={handleLogin}
             className='w-10 h-10 rounded-full bg-[#7289DA] text-white font-bold text-lg flex items-center justify-center'
-            title='Login with Discord'
-            aria-label='Login with Discord'
+            title='Login with Google'
+            aria-label='Login with Google'
         >
             G
         </button>
