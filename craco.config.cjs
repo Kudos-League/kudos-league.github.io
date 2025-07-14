ae65813ced39bd1cdccf0c647f7433dd2b4a7362
Isabella <isabellafj97@gmail.com>
2025-07-13 22:13:03 -0500

    fix craco config

--- craco.config.cjs
+++ craco.config.cjs
@@ -1,10 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 module.exports = {
     webpack: {
         alias: {
             '@': path.resolve(__dirname, 'src')
         }
+    },
+    babel: {
+        plugins: [
+            isDevelopment && 'react-refresh/babel'
+        ].filter(Boolean),
     }
 };
