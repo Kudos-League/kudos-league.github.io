307ef28acadcb281f893e472ac9fe38278d65bbd
Isabella <isabellafj97@gmail.com>
2025-07-31 21:03:53 -0500

    bundle analyzer

--- craco.config.cjs
+++ craco.config.cjs
@@ -1,17 +1,41 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require('path');
+/* eslint-disable @typescript-eslint/no-var-requires */
 
-// const isDevelopment = process.env.NODE_ENV === 'development';
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
     webpack: {
         alias: {
-            '@': path.resolve(__dirname, 'src')
-        }
+            '@': path.resolve(__dirname, 'src'),
+        },
+        configure: (webpackConfig) => {
+            webpackConfig.optimization.minimize = true;
+            webpackConfig.optimization.minimizer = [
+                new TerserPlugin({
+                    terserOptions: {
+                        compress: false,
+                        mangle: false,
+                        format: {
+                            beautify: true,
+                            comments: true,
+                        },
+                    },
+                }),
+            ];
+
+            webpackConfig.plugins.push(
+                new BundleAnalyzerPlugin({
+                    analyzerMode: 'disabled',
+                    generateStatsFile: true,
+                    statsFilename: 'stats.json',
+                })
+            );
+
+            return webpackConfig;
+        },
     },
     babel: {
-        plugins: [
-            // isDevelopment && 'react-refresh/babel'
-        ].filter(Boolean),
-    }
+        plugins: [],
+    },
 };
