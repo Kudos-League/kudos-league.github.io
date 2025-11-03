ed44e68742f9433c818e7052bf7f2ae974ed9870
David Velasco <dvelasco97@protonmail.com>
2025-11-04 00:13:12 +0100

    fix: local button is clearer

--- src/components/events/EventsCarousel.tsx
+++ src/components/events/EventsCarousel.tsx
@@ -183,7 +183,7 @@ export default function EventsCarousel() {
                             : 'bg-gray-300 hover:bg-gray-400'
                     }`}
                 >
-                    {locationFilter ? 'Local (On)' : 'Local (Off)'}
+                    {locationFilter ? 'Show just local events' : 'Show non local events'}
                 </Button>
 
                 {/* <div className='relative'>
