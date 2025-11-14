e2068fd966f7bb2fb3e7ce0aff1a0a2fdd64e681
David Velasco <dvelasco97@protonmail.com>
2025-11-14 04:22:38 +0100

    fix: good conditional logic on showing request this on post details

--- src/components/posts/PostDetails.tsx
+++ src/components/posts/PostDetails.tsx
@@ -931,7 +931,9 @@ export default function PostDetails(props: Props) {
 
                 {postDetails.status !== 'closed' &&
                     user?.id !== Number(postDetails.sender?.id) &&
-                    postDetails.handshakes?.map((h: any) => h.senderID !== user?.id).every((h: any) => h.status === 'cancelled') &&
+                    !postDetails.handshakes?.some((h: any) => 
+                        h.senderID === user?.id && h.status !== 'cancelled'
+                    ) &&
                     (
                         <div className='mt-4 flex justify-center'>
                             <Button
